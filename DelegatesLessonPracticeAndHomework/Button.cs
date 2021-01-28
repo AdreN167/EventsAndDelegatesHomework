a7bbae530c0dfb31ea78afa92757aafa85e67507
AdreN167 <zz11926557@gmail.com>
2021-01-28 18:44:06 +0600

    Домашнее и практическое задание по делегатам

---
+++ DelegatesLessonPracticeAndHomework/Button.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace DelegatesLessonPracticeAndHomework
+{
+    public delegate void PushButton<T>(params T[] arguments);
+
+    public class Button<T>
+    {
+        public string Border { get; set; }
+        public string Text { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public ConsoleColor BorderColor { get; set; }
+        public ConsoleColor FillColor { get; set; }
+        
+        public PushButton<T> PushButton { get; set; }
+
+        public void Display()
+        {
+            var defaultTextColor = Console.ForegroundColor;
+            var defaultBackColor = Console.BackgroundColor;
+
+            Console.ForegroundColor = BorderColor;
+            for (int i = 0; i < Width; i++)
+            {
+                Console.Write(Border);
+            }
+            Console.ForegroundColor = defaultTextColor;
+
+            Console.WriteLine();
+
+            for (int i = 0; i < Height - 2; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    if (j == (Width - Text.Length) / 2 && i == (Height - 2) / 2)
+                    {
+                        Console.BackgroundColor = FillColor;
+                        Console.ForegroundColor = BorderColor;
+                        Console.Write(Text);
+                        Console.ForegroundColor = defaultTextColor;
+                        Console.BackgroundColor = defaultBackColor;
+
+                        j += Text.Length;
+                    }
+                    
+                    if (j == 0 || j == Width - 1)
+                    {
+                        Console.ForegroundColor = BorderColor;
+                        Console.Write(Border);
+                        Console.ForegroundColor = defaultTextColor;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = FillColor;
+                        Console.Write(" ");
+                        Console.BackgroundColor = defaultBackColor;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = BorderColor;
+            for (int i = 0; i < Width; i++)
+            {
+                Console.Write(Border);
+            }
+            Console.ForegroundColor = defaultTextColor;
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DelegatesLessonPracticeAndHomework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DelegatesLessonPracticeAndHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var button = new Button<string>
+            {
+                Border = "*",
+                Text = "Кнопка",
+                Width = 15,
+                Height = 5,
+                BorderColor = ConsoleColor.Red,
+                FillColor = ConsoleColor.Yellow,
+                PushButton = new PushButton<string>(PrintText)
+            };
+
+            button.Display();
+            button.PushButton("Hello", "world", "!");
+        }
+
+        private static void PrintText(params string[] text)
+        {
+            foreach(var row in text)
+            {
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
